f402cfc14c4f37e8a5b6f746c3c90b731be0b3bb
Carlos Del Valle <55850882+cfdelv100@users.noreply.github.com>
2021-11-09 18:36:31 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,25 +1,30 @@
 //import util.Inventory;
+
 import java.util.*;
 import java.util.Scanner;
-public class Menu{
+
+public class Menu {
     //try catch methods and does not terminate when writing the menu
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         logInMenu();
 
     }
-    static void logInMenu(){
+
+    static void logInMenu() {
         // Get inside a loop and user hits 0
-        while(true){
-            String a = "Log In Menu. Enter a number to enter next screen(Q to Quit): ";
-            Scanner in = new Scanner(System.in);
-            System.out.println(a);
+        String a = "Log In Menu. Enter a number to enter next screen(Q to Quit): ";
+        Scanner in = new Scanner(System.in);
+        System.out.println(a);
+        System.out.println("1.Select to Filter Vehicles");
+        System.out.println("2.Select to Sell Vehicle");
+        System.out.println("3.Select to Enter VIN Number");
+        System.out.println("4.Select to Enter User Credentials");
+        while (true) {
+
             String selection = in.nextLine();
             if (!selection.equals("Q")) {
-                System.out.println("1.Select to Filter Vehicles");
-                System.out.println("2.Select to Sell Vehicle");
-                System.out.println("3.Select to Enter VIN Number");
-                System.out.println("4.Select to Enter User Credentials");
+
                 try {
                     switch (selection) {
                         case "1":
@@ -37,33 +42,35 @@ public class Menu{
 
                     }
 
-                    }
-                catch(InputMismatchException ex){
+                } catch (InputMismatchException ex) {
                     System.out.print("***Invalid input***\n");
                     in.nextLine();
                 }
-            }
-            else {
+            } else {
                 System.exit(0);
             }
         }
-        }
-//    static void mainMenu(){
+    }
+
+    //    static void mainMenu(){
 //
 //
 //    }
-    static void filterMenu(){
+    static void filterMenu() {
         System.out.println("Welcome to the Filter Menu! How would you like to filter vehicles? ");
 
     }
-    static void sellMenu(){
+
+    static void sellMenu() {
         System.out.println("Welcome to the Sell Menu!");
     }
-    static void vinMenu(){
+
+    static void vinMenu() {
         System.out.println("Welcome to the Vin Input Screen! Please input VIN Number of Vehicle: ");
 
     }
-    static void userMenu(){
+
+    static void userMenu() {
         System.out.println("User Menu");
 
     }
