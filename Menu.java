726b287b062338340e7d545c4fff4ee7dde72502
Carlos Del Valle <55850882+cfdelv100@users.noreply.github.com>
2021-11-29 20:54:22 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -4,21 +4,24 @@ import util.User;
 import java.util.*;
 import java.util.Scanner;
 
-public class Menu extends User{
-    public Menu(String username, boolean admin) {
-        super(username, admin);
-    }
+public class Menu{
 
-    //try catch methods and does not terminate when writing the menu
+    User user;
+//    public Menu(String username, boolean admin) {
+//        super(username, admin);
+//    }
+
+    /**try catch methods and does not terminate when writing the menu**/
     public static void main(String[] args) {
+        Menu menu = new Menu();
         Scanner first = new Scanner(System.in);
         System.out.println("Input 1 for Admin or 0 for User.");
         int match = first.nextInt();
         try {
            switch (match){
-               case 0: logInMenu();
+               case 0: menu.logInMenu();
                break;
-               case 1: AdminMenu();
+               case 1: menu.AdminMenu();
                break;
                default:
                    System.out.println("***Invalid Input***\n");
@@ -34,7 +37,7 @@ public class Menu extends User{
 
     }
 
-    static void logInMenu() {
+    public void logInMenu() {
         // Get inside a loop and user hits 0
         String a = "Log In Menu. Enter a number to enter next screen(Q to Quit): \n";
         String b = "If an Admin, please hit 9 for admin log in";
@@ -83,9 +86,9 @@ public class Menu extends User{
         }
     }
         //Administration Menu
-        static void AdminMenu(){
+        public void AdminMenu(){
             // Get inside a loop and user hits 0
-            String a = "Admin Menu. Enter a number to enter next screen(Q to Quit): \n";
+            String a = "***Admin Menu. Enter a number to enter next screen(Q to Quit): ***\n";
             Scanner in = new Scanner(System.in);
             System.out.println(a);
             System.out.println("1.Select to Filter Vehicles");
@@ -133,7 +136,7 @@ public class Menu extends User{
 //
 //
 //    }
-    static void filterMenu() {
+    public void filterMenu() {
         System.out.println("Welcome to the Filter Menu! How would you like to filter vehicles? ");
         System.out.println("1.Display All");
         System.out.println("2.Filter Attributes");
@@ -143,7 +146,10 @@ public class Menu extends User{
         try{
             switch(filterSelection) {
                 case 0: logInMenu();
-                case 1: // display all vehicles
+                case 1: Inventory inventory = Inventory.getInventory();
+                        LinkedList<Vehicle> vehicles = inventory.getAllVehicles();// display all vehicles
+                        for(Vehicle vehicle: vehicles)
+                             System.out.println(vehicle);
                 case 2: attributes(); // go into new method with attributes
                 default:
                     System.out.println("***Invalid Input***\n");
@@ -156,13 +162,23 @@ public class Menu extends User{
         }
 
     }
-    static void attributes(){
+    public void attributes(){
         //method to further filter vehicles
-
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        Inventory inventory = Inventory.getInventory();
+        if(i == 1){
+            LinkedList<Vehicle> vehicles = inventory.getFilteredVehicles("model","size");
+//            for ();
+        }
+        else if(i == 0){
+            logInMenu();
+        }
     }
 
-    static void sellMenu() {
-        System.out.println("Welcome to the Sell Menu!");
+    public void sellMenu() {
+        System.out.println("***Welcome to the Sell Menu!***");
         System.out.println("Enter \"1\" for Listings");
         Scanner input = new Scanner(System.in);
         int i = input.nextInt();
@@ -176,18 +192,29 @@ public class Menu extends User{
 //        if(input.nextInt() == 2){
 //            User user = inventory.addCar();
 //        }
+
+
+        if(i == 0){
+            logInMenu();
+        }
     }
 
-    static void vinMenu() {
-        System.out.println("Welcome to the Vin Input Screen! Please input VIN Number of Vehicle: ");
+    public void vinMenu() {
+        System.out.println("***Welcome to the Vin Input Screen! Please input VIN Number of Vehicle: ***");
         Scanner in = new Scanner(System.in);
         int vin = in.nextInt();
         //conditional statement to compare if VIN is valid within the db,
         //else return error message, maybe a try catch block
+
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        if(i == 0){
+            logInMenu();
+        }
     }
 
-    static void userMenu() {
-        System.out.println("User Menu");
+    public void userMenu() {
+        System.out.println("***User Menu***");
         //Methods to activate and deactivate
         Inventory inv = Inventory.getInventory();
         inv.addUser("user","password",false);
@@ -195,24 +222,41 @@ public class Menu extends User{
         for (String s: inv.listUser()) {
             System.out.println(s);
         }
+
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        if(i == 0){
+            logInMenu();
+        }
     }
-    static void displayMenu() {
-        System.out.println("Display Users");
-        //Methods to display users
+    public void displayMenu() {
+        /**Methods to display users**/
+        System.out.println("***Displaying Users***");
         Inventory inv = Inventory.getInventory();
         for (String s: inv.listUser()) {
             System.out.println(s);
         }
 
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        if(i == 0){
+            logInMenu();
+        }
+
     }
-    static void adminLogIn(){
+    public void adminLogIn(){
         System.out.print("Admin Log in");
         Scanner admin = new Scanner(System.in);
         String login = admin.nextLine();
         //TODO: Implement database for login for admin
         Inventory inv = Inventory.getInventory();
-        User user = inv.checkUser("root","admin");
+        user = inv.checkUser("root","admin");
 
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        if(i == 0){
+            logInMenu();
+        }
 
     }
 
